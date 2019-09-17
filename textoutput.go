e60aa0a6e570ac6f9b620cc90e91dc3984a2a980
Alexander F. Rødseth <rodseth@gmail.com>
2019-09-17 19:31:16 +0200

    Refactor

--- textoutput.go
+++ textoutput.go
@@ -2,6 +2,7 @@
 package textoutput
 
 import (
+	"fmt"
 	"github.com/xyproto/vt100"
 	"os"
 )
@@ -139,4 +140,27 @@ func (o *TextOutput) Words(line string, colors ...string) string {
 		return vt100.Words(line, colors...)
 	}
 	return line
+}
+
+// Write a message to stdout if output is enabled
+func (o *TextOutput) Println(msg ...interface{}) {
+	if o.enabled {
+		fmt.Println(msg...)
+	}
+}
+
+// Change the color state in the terminal emulator
+func (o *TextOutput) ColorOn(attribute1, attribute2 int) string {
+	if !o.color {
+		return ""
+	}
+	return fmt.Sprintf("\033[%d;%dm", attribute1, attribute2)
+}
+
+// Change the color state in the terminal emulator
+func (o *TextOutput) ColorOff() string {
+	if !o.color {
+		return ""
+	}
+	return "\033[0m"
 }
